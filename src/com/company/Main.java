7d4072cde94f5554b0be4c054399381e4dce4415
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2021-09-09 19:54:22 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    //    We can get the documentation in intelliJ by cntrl + J in Mac Cntrl + q in windows
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello World");
+        System.out.println("test sout");
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("Welcome")
+            .append(" to")
+            .append(" the world");
+
+        System.out.println(sb);
+    }
+}
