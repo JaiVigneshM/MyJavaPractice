a4f1558cb8fa7d2c4c40e797c229adb479d7a197
JaiVigneshM <50944351+JaiVigneshM@users.noreply.github.com>
2021-09-09 20:00:48 -0400

    test branch split

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,17 @@ package com.company;
 
 public class Main {
 
-    //    We can get the documentation in intelliJ by cntrl + J in Mac Cntrl + q in windows
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World");
-        System.out.println("test sout");
+  //    We can get the documentation in intelliJ by cntrl + J in Mac Cntrl + q in windows
+  public static void main(String[] args) {
+    // write your code here
+    System.out.println("Hello World");
+    System.out.println("test ");
 
-        StringBuilder sb = new StringBuilder();
-        sb.append("Welcome")
-            .append(" to")
-            .append(" the world");
-
-        System.out.println(sb);
-    }
+    StringBuilder sb = new StringBuilder();
+    sb.append("Welcome")
+        .append(" to")
+        .append(" the world");
+    String s = sb.toString();
+    System.out.println(s);
+  }
 }
